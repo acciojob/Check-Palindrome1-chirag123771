54600a3e3546b39da67e929a48832f831bd85c18
Chirag Agarwal <72219269+chirag123771@users.noreply.github.com>
2023-04-05 15:31:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // complete the given function
 
 function palindrome(str){
-
+  str = str.replace(/[^a-z0-9]/gi, '').toLowerCase();
+  // Compare the string with its reverse
+  return str === str.split('').reverse().join('');
 }
 module.exports = palindrome
